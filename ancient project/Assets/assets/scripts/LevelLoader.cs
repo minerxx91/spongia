b9c761da2afb562a62a55d42650631751513fba2
NikolasMichalicka <michalickanikolas@gmail.com>
2022-10-23 18:09:33 +0200

    SceneLoader

---
+++ ancient project/Assets/assets/scripts/LevelLoader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        if(SceneManager.GetActiveScene().buildIndex != 0) StartCoroutine(LoadLevel(0));
+        else StartCoroutine(LoadLevel(1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+
+
+
+
+
+
+
+
+}
--- ancient project/Assets/assets/scripts/cameraFollow.cs
+++ ancient project/Assets/assets/scripts/cameraFollow.cs
@@ -13,7 +13,7 @@ public class cameraFollow : MonoBehaviour
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime * 5);
         transform.position = smoothedPosition;
 
-        //transform.LookAt(target);
+        transform.LookAt(target);
 
 
     }
