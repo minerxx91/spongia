393fe278964ea972040f0592775cc8ce685ddf13
Krypt0niT <k269692@gmail.com>
2022-10-29 23:23:32 +0200

    ui10

--- ancient project/Assets/assets/scripts/MousePosition3D.cs
+++ ancient project/Assets/assets/scripts/MousePosition3D.cs
@@ -34,7 +34,6 @@
 
                     
                 }
-                print(managerVariables.Player.target);
 
 
             }
--- ancient project/Assets/assets/scripts/UI.cs
+++ ancient project/Assets/assets/scripts/UI.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
@@ -28,7 +30,7 @@ public class UI : MonoBehaviour
         controls = managerVariables.GetComponent<Controls>();
 
         player = GameObject.Find("Player").GetComponent<Player>();
-
+        
 
 
 
@@ -48,11 +50,32 @@ private void Update()
         float dielikS = CircleInventorySlot / managerVariables.Player.MaxStamina;
         StaminaMask.transform.position = new Vector2(StaminaMask.transform.position.x, (managerVariables.Player.Stamina * dielikS) - 35);
 
+
         float dielikJ = BasicInventorySlot / managerVariables.Player.JumpCooldown;
         JumpColor.transform.position = new Vector2(JumpColor.transform.position.x, (player.JumpCooldown * dielikJ)- 12.5f);
+        if(managerVariables.Player.JumpCooldown == player.JumpCooldown)
+        {
+            JumpColor.GetComponent<Image>().color = new Color32(255, 180, 0, 100);
+        }
+        else
+        {
+            JumpColor.GetComponent<Image>().color = new Color32(178, 128, 45, 100);
+
+        }
 
         float dielikA = BasicInventorySlot / managerVariables.Player.AttackCooldown;
         AttackColor.transform.position = new Vector2(AttackColor.transform.position.x, (player.AttackCooldown * dielikA) - 12.5f);
+        if (managerVariables.Player.AttackCooldown == player.AttackCooldown)
+        {
+            AttackColor.GetComponent<Image>().color = new Color32(255, 180, 0, 100);
+        }
+        else
+        {
+            AttackColor.GetComponent<Image>().color = new Color32(178, 128, 45, 100);
+
+        }
+
+
         if (GameObject.FindGameObjectsWithTag("Boss").Length != 0)
         {
             float bossHealth = GameObject.FindGameObjectsWithTag("Boss")[0].GetComponent<EnemyNavMesh>().Health;
@@ -61,9 +84,14 @@ private void Update()
             BossBar.transform.localScale = new Vector2((bossHealth * dielikBoss) /1800, BossBar.transform.localScale.y);
             
         }
+
+
+
         
         
 
        
     }
+ 
+
 }
