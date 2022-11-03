c772267c73b2e445d6acbeae19ba87756a47f817
Krypt0niT <k269692@gmail.com>
2022-11-03 17:36:07 +0100

    rozpoznavanie tridentu

--- ancient project/Assets/assets/scripts/PoseidonAttack.cs
+++ ancient project/Assets/assets/scripts/PoseidonAttack.cs
@@ -23,32 +23,60 @@ public class PoseidonAttack : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            if (managerVariables.Player.Health > managerVariables.Poseidon.Damage + managerVariables.Poseidon.DamageIncrease)
+            if (this.gameObject.name == "AttackMelee2" || gameObject.name == "AttackMelee1")
             {
-                managerVariables.Player.Health -= (managerVariables.Poseidon.Damage + managerVariables.Poseidon.DamageIncrease)*(100- managerVariables.Player.Resistence) /100;
-                if(managerVariables.Player.Resistence > 0)
+                print("melee");
+                if (managerVariables.Player.Health > managerVariables.Poseidon.Damage + managerVariables.Poseidon.DamageIncrease)
                 {
-                    if (managerVariables.Player.ShieldStaminaCost <= managerVariables.Player.Stamina)
+                    managerVariables.Player.Health -= (managerVariables.Poseidon.Damage + managerVariables.Poseidon.DamageIncrease) * (100 - managerVariables.Player.Resistence) / 100;
+                    if (managerVariables.Player.Resistence > 0)
                     {
-                        audioManager.PlayPlayerShield();
-                        Invoke(nameof(ShieldDown), .3f);
-                        managerVariables.Player.absorb2 = true;
-                        managerVariables.Player.Stamina -= managerVariables.Player.ShieldStaminaCost;
+                        if (managerVariables.Player.ShieldStaminaCost <= managerVariables.Player.Stamina)
+                        {
+                            audioManager.PlayPlayerShield();
+                            Invoke(nameof(ShieldDown), .3f);
+                            managerVariables.Player.absorb2 = true;
+                            managerVariables.Player.Stamina -= managerVariables.Player.ShieldStaminaCost;
+                        }
+
                     }
-                       
+                    else managerVariables.Player.absorb = true;
                 }
-                else managerVariables.Player.absorb = true;
+                else
+                {
+                    managerVariables.Player.absorb = true;
+                    managerVariables.Player.Health = 0;
+                }
+
+
             }
-            else
+            if (this.gameObject.name == "EnemyTrident")
             {
-                managerVariables.Player.absorb = true;
-                managerVariables.Player.Health = 0;
+                print("trident");
+                if (managerVariables.Player.Health > managerVariables.Poseidon.Damage + managerVariables.Poseidon.DamageIncrease)
+                {
+                    managerVariables.Player.Health -= (managerVariables.Poseidon.Damage + managerVariables.Poseidon.DamageIncrease) * (100 - managerVariables.Player.Resistence) / 100;
+                    if (managerVariables.Player.Resistence > 0)
+                    {
+                        if (managerVariables.Player.ShieldStaminaCost <= managerVariables.Player.Stamina)
+                        {
+                            audioManager.PlayPlayerShield();
+                            Invoke(nameof(ShieldDown), .3f);
+                            managerVariables.Player.absorb2 = true;
+                            managerVariables.Player.Stamina -= managerVariables.Player.ShieldStaminaCost;
+                        }
+
+                    }
+                    else managerVariables.Player.absorb = true;
+                }
+                else
+                {
+                    managerVariables.Player.absorb = true;
+                    managerVariables.Player.Health = 0;
+                }
+
+
             }
-            /*if ( managerVariables.Player.AttackInProcess)
-            {
-                
-                
-            }*/
 
         }
     }
