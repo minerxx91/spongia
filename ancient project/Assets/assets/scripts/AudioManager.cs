c71f03932150b0d616116de77bab791bd6efad45
Krypt0niT <k269692@gmail.com>
2022-11-06 13:07:11 +0100

    audio manager

--- ancient project/Assets/assets/scripts/AudioManager.cs
+++ ancient project/Assets/assets/scripts/AudioManager.cs
@@ -55,11 +55,8 @@ public class AudioManager : MonoBehaviour
     AudioSource[] AS;
     private void Start()
     {
-        AS = GetComponents<AudioSource>();
 
-        run = AS[1];
-        MusicList = AS[3];
-        print(AS[3]);
+
         if (Audiomanager_d != null)
         {
             Destroy(this.gameObject);
@@ -70,6 +67,14 @@ public class AudioManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+
+
+        AS = GetComponents<AudioSource>();
+
+        run = AS[1];
+        MusicList = AS[3];
+        print(AS[3]);
+        
     }
     private void Update()
     {
